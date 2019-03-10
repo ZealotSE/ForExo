f876da1a23a76a4bb73ac7dc0628b7a78f25de9b
ZealotSE <sliski69@gmail.com>
2019-03-10 16:55:58 +0100

    Stworzenie klasy kostki

---
+++ ExoTests/RollThisDice/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollThisDice
+{
+    class Dice
+    {
+        private static Random rand = new Random();
+        private int[] PossibleValues;
+        public int Value { get; private set; }
+
+        public Dice()
+        {
+            PossibleValues = new int[] { 1, 2, 3, 4, 5, 6 };
+            Value = rand.Next(1, 7);
+            Console.WriteLine("Created dice");
+        }       
+
+        public void Roll()
+        {            
+            Value = PossibleValues[rand.Next(0, 6)];            
+        }
+    }
+}
