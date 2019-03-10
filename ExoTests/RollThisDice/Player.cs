ac681806ac06b0eb63e97b269ea171669e8f7396
ZealotSE <sliski69@gmail.com>
2019-03-10 17:10:42 +0100

    Dodanie klasy gracza

---
+++ ExoTests/RollThisDice/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RollThisDice
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Points = 0;
+            Console.WriteLine("Created player");
+        }
+
+        public void AddPoints(int points)
+        {
+            Points += points;
+        }
+
+        public int RollDice(Dice dice)
+        {
+            dice.Roll();
+            return dice.Value;
+        }
+    }
+}
